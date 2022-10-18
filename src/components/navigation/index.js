1b28e83c72ad1bb7b8e327d7543fcf257ea8d57f
JoseAnnunziatoSonos <Jose.Annunziato@sonos.com>
2022-10-18 09:36:18 -0400

    clean up navigation

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -17,26 +17,26 @@ function Navigation() {
     {label: 'More', icon: 'fa-circle-ellipsis', path: '/more'},
     {label: 'Login', icon: 'fa-user', path: '/login'},
   ]
-  return(
+  return (
     <div className="ttr-navigation">
-     <ul className="list-group">
-      {
-      links.map((link, ndx) => {
-        return(
-        <li key={ndx} className={`list-group-item border-0 ttr-font-size-150pc text-nowrap
-         ${pathname.indexOf(link.path) >= 0 ? 'fw-bold':''}`}>
-          <Link to={link.path} id={link.label}
-             className="text-decoration-none text-black">
-            <i className={`fa ${link.icon} text-center`}></i>
-            <span className="ttr-label">{link.label}</span>
-          </Link>
-        </li>
-        );
-      })
-      }
-      </ul>
-     <a href="#" className="mt-3 btn btn-lg btn-primary rounded-pill w-100 fw-bold text-white">
-      Tuit</a>
+      <div className="list-group">
+        {
+          links.map((link, ndx) => {
+            return (
+              <Link
+                to={link.path} id={link.label}
+                key={ndx}
+                className={`list-group-item border-0 ttr-font-size-150pc text-nowrap
+                          ${pathname.indexOf(link.path) >= 0 ? 'active' : ''}`}>
+                <i className={`fa ${link.icon} text-center`}></i>
+                <span className="ttr-label">{link.label}</span>
+              </Link>
+            );
+          })
+        }
+      </div>
+      <a href="#" className="mt-3 btn btn-lg btn-primary rounded-pill w-100 fw-bold text-white">
+        Tuit</a>
     </div>
   );
 };
