eca45ab696cdfd960924c5ec197e88bc66a58032
liuyang <yang.liu2@northeastern.edu>
2022-11-20 17:31:26 -0500

    fix problem

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -17,6 +17,7 @@ function Navigation() {
     {label: 'Movies', icon: 'fa-user', path: '/movies'},
     {label: 'More', icon: 'fa-circle-ellipsis', path: '/more'},
     {label: 'Login', icon: 'fa-user', path: '/login'},
+    {label: 'Signup', icon: 'fa-user', path: '/signup'},
   ];
   return (
     <div className="ttr-navigation">
--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import './tuits.css';
 import Tuit from "./tuit";
+import * as likesService from "../../services/likes-service";
+import * as dislikesService from "../../services/dislikes-service";
 
-function Tuits({tuits = [], deleteTuit}) {
+function Tuits({tuits = [], deleteTuit, refreshTuits}) {
+    const likeTuit = (tuit) =>
+        likesService
+            .userTogglesTuitLikes("me", tuit._id)
+            .then(refreshTuits)
+            .catch(e => alert(e))
+    const dislikeTuit = (tuit) =>
+        dislikesService
+            .userTogglesTuitDislikes("me", tuit._id)
+            .then(refreshTuits)
+            .catch(e => alert(e))
     return (
     <div>
       <ul className="ttr-tuits list-group">
