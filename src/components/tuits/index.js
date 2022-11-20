c7d5916b15a31195f09ecb3fd8df84ad8219fd85
liuyang <yang.liu2@northeastern.edu>
2022-11-20 14:37:04 -0500

    add dislike

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -1,9 +1,16 @@
 import Tuit from "./tuit";
 import * as likesService from "../../services/likes-service";
+import * as dislikesService from "../../services/dislikes-service";
+
 const Tuits = ({tuits = [], deleteTuit, refreshTuits}) => {
     const likeTuit = (tuit) =>
         likesService
             .userTogglesTuitLikes("me", tuit._id)
+            .then(refreshTuits)
+            .catch(e => alert(e))
+    const dislikeTuit = (tuit) =>
+        dislikesService
+            .userTogglesTuitDislikes("me", tuit._id)
             .then(refreshTuits)
             .catch(e => alert(e))
 
@@ -15,35 +22,11 @@ const Tuits = ({tuits = [], deleteTuit, refreshTuits}) => {
                         <Tuit key={tuit._id}
                               deleteTuit={deleteTuit}
                               likeTuit={likeTuit}
+                              dislikeTuit={dislikeTuit}
                               tuit={tuit}/>)
                 }
             </ul>
         </div>
     );
 }
-
 export default Tuits;
-
-// import React from "react";
-// import './tuits.css';
-// import Tuit from "./tuit";
-//
-// function Tuits({tuits = [], deleteTuit}) {
-//     return (
-//     <div>
-//       <ul className="ttr-tuits list-group">
-//         {
-//           tuits.map && tuits.map(tuit => {
-//             return (
-//               <Tuit key={tuit._id}
-//                     deleteTuit={deleteTuit}
-//                     tuit={tuit}/>
-//             );
-//           })
-//         }
-//       </ul>
-//     </div>
-//   );
-// }
-//
-// export default Tuits;
