8baef64319601893a6312ba11bf889117a8cfd7d
liuyang <yang.liu2@northeastern.edu>
2022-11-20 15:21:28 -0500

    update login, media, dislike, like

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-
 const TUITS_API = `${BASE_URL}/api/tuits`;
 const USERS_API = `${BASE_URL}/api/users`;
 
@@ -25,7 +24,6 @@ export const findTuitById = (tid) =>
   axios.get(`${TUITS_API}/${tid}`)
     .then(response => response.data);
 
-
 // export const createTuit = (uid, tuit) =>
 //   axios.post(`${USERS_API}/${uid}/tuits`, tuit)
 //     .then(response => response.data);
@@ -38,32 +36,3 @@ export const deleteTuit = (tid) =>
   axios.delete(`${TUITS_API}/${tid}`)
     .then(response => response.data);
 
-// import axios from "axios";
-// const BASE_URL = "http://my-node-express-project-env.eba-hxq4pgvm.us-east-1.elasticbeanstalk.com"
-// //const BASE_URL = "https://my-tuit-project.herokuapp.com";
-// const TUITS_API = `${BASE_URL}/api/tuits`;
-// const USERS_API = `${BASE_URL}/api/users`;
-//
-// export const findAllTuits = () =>
-//   axios.get(TUITS_API)
-//     .then(response => response.data);
-//
-// export const findTuitById = (tid) =>
-//   axios.get(`${TUITS_API}/${tid}`)
-//     .then(response => response.data);
-//
-// export const findTuitByUser = (uid) =>
-//   axios.get(`${USERS_API}/${uid}/tuits`)
-//     .then(response => response.data);
-//
-// export const createTuit = (uid, tuit) =>
-//   axios.post(`${USERS_API}/${uid}/tuits`, tuit)
-//     .then(response => response.data);
-//
-// export const updateTuit = (tid, tuit) =>
-//   axios.post(`${TUITS_API}/${tid}`, tuit)
-//     .then(response => response.data);
-//
-// export const deleteTuit = (tid) =>
-//   axios.delete(`${TUITS_API}/${tid}`)
-//     .then(response => response.data);
