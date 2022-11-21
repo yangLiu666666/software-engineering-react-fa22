a7f242dfbc3407d7c49d2cd539964a0d3fb9412a
liuyang <yang.liu2@northeastern.edu>
2022-11-20 22:43:32 -0500

    fix bug

---
+++ src/components/profile/my-dislikes.js
@@ -0,0 +1,37 @@
+// import Tuits from "../tuits";
+// import * as service from "../../services/dislikes-service";
+// import {useEffect, useState} from "react";
+//
+// const MyDislikes = () => {
+//     const [dislikedTuits, setDislikedTuits] = useState([]);
+//     const findTuitsIDislike = () =>
+//         service.findAllTuitsDislikedByUser("me")
+//             .then((tuits) => setDislikedTuits(tuits));
+//     useEffect(findTuitsIDislike, []);
+//
+//     return(
+//         <div>
+//             <Tuits tuits={dislikedTuits} refreshTuits={findTuitsIDislike}/>
+//         </div>
+//     );
+// };
+// export default MyDislikes;
+import Tuits from "../tuits";
+import * as service from "../../services/dislikes-service";
+import {useEffect, useState} from "react";
+
+const MyDislikes = () => {
+    const [dislikedTuits, setDislikedTuis] = useState([]);
+    const findTuitsIDislike = () =>
+        service.findAllTuitsDislikedByUser("me")
+            .then((tuits) => setDislikedTuis(tuits));
+    useEffect(findTuitsIDislike, []);
+
+    return(
+        <div>
+            <Tuits tuits={dislikedTuits}
+                   refreshTuits={findTuitsIDislike}/>
+        </div>
+    );
+};
+export default MyDislikes;
--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,29 +1,39 @@
 import axios from "axios";
-const BASE_URL = "http://my-node-express-project-env.eba-hxq4pgvm.us-east-1.elasticbeanstalk.com"
+//const BASE_URL = "http://my-node-express-project-env.eba-hxq4pgvm.us-east-1.elasticbeanstalk.com"
 //const BASE_URL = "https://my-tuit-project.herokuapp.com";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 const TUITS_API = `${BASE_URL}/api/tuits`;
 const USERS_API = `${BASE_URL}/api/users`;
 
+const api = axios.create({
+    withCredentials: true
+});
+
 export const findAllTuits = () =>
-  axios.get(TUITS_API)
-    .then(response => response.data);
+    axios.get(TUITS_API)
+        .then(response => response.data);
 
 export const findTuitById = (tid) =>
-  axios.get(`${TUITS_API}/${tid}`)
-    .then(response => response.data);
+    axios.get(`${TUITS_API}/${tid}`)
+        .then(response => response.data);
 
-export const findTuitByUser = (uid) =>
-  axios.get(`${USERS_API}/${uid}/tuits`)
-    .then(response => response.data);
+export const findTuitsByUser = (uid) =>
+    api.get(`${USERS_API}/${uid}/tuits`)
+        .then(response => response.data);
 
 export const createTuit = (uid, tuit) =>
-  axios.post(`${USERS_API}/${uid}/tuits`, tuit)
-    .then(response => response.data);
+    axios.post(`${USERS_API}/${uid}/tuits`, tuit)
+        .then(response => response.data);
+
+export const createTuitByUser = (uid, tuit) =>
+    api.post(`${USERS_API}/${uid}/tuits`, tuit)
+        .then(response => response.data);
 
 export const updateTuit = (tid, tuit) =>
-  axios.post(`${TUITS_API}/${tid}`, tuit)
-    .then(response => response.data);
+    axios.post(`${TUITS_API}/${tid}`, tuit)
+        .then(response => response.data);
 
 export const deleteTuit = (tid) =>
-  axios.delete(`${TUITS_API}/${tid}`)
-    .then(response => response.data);
+    axios.delete(`${TUITS_API}/${tid}`)
+        .then(response => response.data);
+
